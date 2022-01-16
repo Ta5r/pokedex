6016e4e357ca0279e5a7b92811a6cdd35f8be098
Ta5r <srv.tanay@gmail.com>
2022-01-16 15:43:26 +0530

    REMOVE : additional redundant files

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,17 +1,58 @@
-import React, {useState} from "react";
-import PokemonList from "./PokemonList";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
+import PokemonThumbnails from "./components/PokemonThumbnails";
+// import PokemonDetails from './components/PokemonDetails'
 
-function App(){
-    const [pokemon, setPokemon] = useState([]);
+const App = () => {
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
+  );
 
-    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
-        setPokemon(res.data.results.map(p => p.name));
-    })
-    
-    return(
-        <PokemonList pokemon={pokemon} />
-    );
-}
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore);
+    const data = await res.json();
+
+    setLoadMore(data.next);
+
+    function createPokemonObject(results) {
+      results.forEach(async (pokemon) => {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        const data = await res.json();
+        setAllPokemons((currentList) => [...currentList, data]);
+        await allPokemons.sort((a, b) => a.id - b.id);
+      });
+    }
+    createPokemonObject(data.results);
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
+  return (
+    <div className="app-contaner">
+      <h1>Pokemon Evolution</h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+          {allPokemons.map((pokemonStats, index) => (
+            <PokemonThumbnails
+              key={index}
+              id={pokemonStats.id}
+              image={pokemonStats.sprites.other.dream_world.front_default}
+              name={pokemonStats.name}
+              type={pokemonStats.types[0].type.name}
+            />
+          ))}
+        </div>
+
+        <button className="load-more" onClick={() => getAllPokemons()}>
+          Load more
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- pokedex/src/index.js
+++ pokedex/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App3 from './App3';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App3 />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
